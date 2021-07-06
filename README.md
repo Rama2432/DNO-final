422795ca7be4fe991c99b49f7005d1f945b2e78f
Rama Javier Ramírez <53785254+Rama2432@users.noreply.github.com>
2021-07-06 19:00:46 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,93 @@
+# Proyecto: KISSIN' CHILE
+
+## ¿Para qué? 
+→ Objetivo general <br>
+→ Reunir el material de KISS en Chile de los fanaticos junto a los proyectos realizados por la comunidad de fanaticos (podcast, libros, grupos y foros) siendo un lugar de encuentro para la comunidad permitiendo compartir, comentar y explorar material exclusivo de cada fanatico.
+
+## ¿Para quiénes? 
+**Fan Experto**   
+![FanExperto](/fotos2/ProtoA.webp)
+<br>
+<br>
+**Fan Regular**
+<br>
+![FanRegular](/fotos2/ProtoB.webp)
+<br>
+<br>
+**Joven Rockero**
+<br>
+![FanRockero](/fotos2/ProtoC.webp)    
+
+## Antecedentes 
+<br>
+
+![web1](/fotos2/web1.webp)
+<br>
+    → https://galeriamlociny.pl/ - Se destaca la organización de elemento al recorrer la landing page con harta información, un poco saturado pero destcando con un gamade  colores que funciona y distigue cada zona por su configuracion en el espacio. <br>
+    <br>
+
+![web2](/fotos2/web2.webp)
+<br>
+    → https://blackmadre.com/ - Tiene un recorrido lineal bien configurado atractivo y entretenido, creo que falta un poco de libertad para explorar. <br>
+    <br>
+
+![web3](/fotos2/web3.webp)
+<br>
+    → https://covidartmuseum.com/ - Inicia de forma dinamica y se detaca la forma de exhibir la variedad de proyectos para agrergar valor en la landing page sin saturar de colores o información, se podria agreggar un poco de aire para aumentar la sensación de desplazamiento pero lo destaco como un buen antecedentes. <br>
+    <br>
+
+![web4](/fotos2/web4.webp)
+<br>
+    → https://cinderblocinc.com/10princearthur/ - Un recorrido inicial que destaca, tiene una variación con el menu que entrega una mirada productiva de la pagina guiando anticipadamente a cada subpage, creo que falta un poco de introducción al ambiente de la pagina. <br>
+    <br>
+
+![web5](/fotos2/web5.webp)
+<br>
+    → https://www.weflowexperience.com/ - Aprovecha mucho la composición de las fotos siendo fotos sin una preparación para estar en la pagina siendo un recurso creativo muy interesante. <br>
+    <br>
+
+![web6](/fotos2/web6.webp)
+<br>
+    → https://www.artcal.co/melbourne - Web estilo de calendario que simplifica el uso de la pagina pero que cada fecha posee un elemento distintivo utilizando a favor disponer de la composicion de un calendario <br>
+    <br>
+    --- Páginas actuales de Kiss Chile --- <br>
+    "Las páginas de KISS ARMY de Chile estan enfocadas a noticias de la banda y actividades del grupo fundador como una entidad cerrada debido a la lucha entre agrupaciones de fanáticos, se propone crear un nuevo lugar enfocado en las vivencias de los fans que permita mostrar contenido de la mirada del público" <br>
+    → https://www.kissarmy.cl/<br>
+    → https://www.kissarmychile.cl/ <br>
+
+
+## Referentes 
+<br>
+
+![web7](/fotos2/web7.webp)
+<br>
+    → https://bridge7.qodeinteractive.com/?_ga=2.165766845.2085471245.1605517916-1590212745.1605323601 - Web de recorrido simple pero con una estetica clara y atractiva que causa interes y se relaciona al mundo de los espectaculos  <br>
+    <br>
+
+![web8](/fotos2/web8.webp)
+<br>
+    → https://rocka.co/ - Web duotone con imagenes flotando, se destaca por su uso de png y una font con actitud que se logra valorar el peso de la fuente rellenando espacio en la misma pagina <br>
+    <br>
+
+![web9](/fotos2/web9.webp)
+<br>
+    → https://www.remijousselme.com/ - Tambien web duotone pero esta se destaca por su composicion desestructurada combinando la presencia de images y texto de forma atractiva que aprovecha la presencia de espacio como un recurso que entrega calidad <br>
+    <br>
+
+![web10](/fotos2/web10.webp)
+<br>
+    → https://dangelicoguitars.com/ -  Web con estetica de guitarra old school que se aprovecha en la tipografia, gama de colores y destaca el uso de fotografias simples con texto para agrergar valor y contenido por cada sección <br>
+    <br>
+
+![web11](/fotos2/web11.webp)
+<br>
+    → https://www.awdagency.com/about/ - Web de recorrido lineal que se destaca por la presencia de un mouse diferente que interactua con la misma web con efectos muy interesantes que logran destacar el contenido a pesar de ser muy notorios no opacan el contenido principal siendo texto y fotos <br>
+    <br>
+
+![web12](/fotos2/web12.webp)
+<br>
+    → https://x.cristobalbalenciagamuseoa.com/en - Web de recorrido lineal y despues lateral que crea un efecto de carrusel interesante para exhibir contenido <br>
+<br>
+    →MOODBOARD: Se expone la dinamica de fotos y colores que se relacionan con el mundo del espectáculo <br>
+    <br>
+    ![MoodBoard de KISS](/fotos2/MoodBoard.webp)
--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,134 @@
-/*!
-* Start Bootstrap - Stylish Portfolio v6.0.1 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', event => {
-
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
-    let scrollToTopVisible = false;
-    // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
-        event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
-        _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
-    scrollTriggerList.map(scrollTrigger => {
-        scrollTrigger.addEventListener('click', () => {
-            sidebarWrapper.classList.remove('active');
-            menuToggle.classList.remove('active');
-            _toggleMenuIcon();
-        })
-    });
-
-    function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-times');
-        if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-times');
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector("#mainNav");
+        if (!navbarCollapsible) {
+            return;
         }
-        if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-times');
-            menuToggleTimes.classList.add('fa-bars');
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove("navbar-shrink");
+        } else {
+            navbarCollapsible.classList.add("navbar-shrink");
         }
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener("scroll", navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector("#mainNav");
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: "#mainNav",
+            offset: 74,
+        });
     }
 
-    // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
-        if (document.documentElement.scrollTop > 100) {
-            if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
-                scrollToTopVisible = true;
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
+    const responsiveNavItems = [].slice.call(document.querySelectorAll("#navbarResponsive .nav-link"));
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
+                navbarToggler.click();
             }
-        } else {
-            if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
-                scrollToTopVisible = false;
-            }
-        }
+        });
+    });
+});
+
+//La API
+fetch("https://sheetdb.io/api/v1/6mk7l08x2hj05")
+    .then((response) => {
+        return response.json();
     })
-})
+    .then((data) => {
+        console.log(data);
+        var portfolio = data;
 
-function fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+        portfolio.forEach(function (trabajo) {
+            if (trabajo.tipo.includes("1994")) {
+                especialidad = '1994';} 
+                else if (trabajo.tipo.includes("1997")) {
+                    especialidad = '1997';} 
+                else if (trabajo.tipo.includes("2009")) {
+                    especialidad = '2009';} 
+                else if (trabajo.tipo.includes("2012")) {
+                    especialidad = '2012';} 
+                else if (trabajo.tipo.includes("2015")) {
+                    especialidad = '2015';} 
+                else {
+                    especialidad = 'none year';
+            }
+        // Tarjetones
+            document.getElementById("portfolio").innerHTML +=
+                '<div class="col-sm-6 col-lg-4 col-xxl-3"' + especialidad + '><div class="card shadow-sm"><img src="' +
+                trabajo.image +
+                '" class="card-img-top" alt="' +
+                trabajo.title +
+                '"><div class="card-body"><h5 class="card-title">' +
+                trabajo.title +
+                '</h5><p class="card-text">' +
+                trabajo.text +
+                '</p> <a href="' +
+                trabajo.video +
+                '" class="btn btn-primary">"Ver más"</a></div></div></div>"';
+        });
 
-function fadeIn(el, display) {
-    el.style.opacity = 0;
-    el.style.display = display || "block";
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+        // Buscador
+
+        // if (document.body.classList.contains("anios")) {
+        //     data.features.forEach(function (anios, i) {}
+
+        $("#opciones").change(function (){
+            valor = $("input[name='opciones']:checked").val();
+            if (valor == "1994") {
+                $(".1994").fadeTo("slow", 1);
+                $(".1997").fadeTo("slow", 0.1);
+                $(".2009").fadeTo("slow", 0.1);
+                $(".2012").fadeTo("slow", 0.1);
+                $(".2015").fadeTo("slow", 0.1);
+            } else if (valor == "1997") {
+                $(".1994").fadeTo("slow", 0.1);
+                $(".1997").fadeTo("slow", 1);
+                $(".2009").fadeTo("slow", 0.1);
+                $(".2012").fadeTo("slow", 0.1);
+                $(".2015").fadeTo("slow", 0.1);
+            } else if (valor == "2009") {
+                $(".1994").fadeTo("slow", 0.1);
+                $(".1997").fadeTo("slow", 0.1);
+                $(".2009").fadeTo("slow", 1);
+                $(".2012").fadeTo("slow", 0.1);
+                $(".2015").fadeTo("slow", 0.1);
+            } else if (valor == "2012") {
+                $(".1994").fadeTo("slow", 0.1);
+                $(".1997").fadeTo("slow", 0.1);
+                $(".2009").fadeTo("slow", 0.1);
+                $(".2012").fadeTo("slow", 1);
+                $(".2015").fadeTo("slow", 0.1);
+            } else if (valor == "2015") {
+                $(".1994").fadeTo("slow", 0.1);
+                $(".1997").fadeTo("slow", 0.1);
+                $(".2009").fadeTo("slow", 0.1);
+                $(".2012").fadeTo("slow", 0.1);
+                $(".2015").fadeTo("slow", 1);
+            } else {
+                $(".1994").fadeTo("slow", 1);
+                $(".1997").fadeTo("slow", 1);
+                $(".2009").fadeTo("slow", 1);
+                $(".2012").fadeTo("slow", 1);
+                $(".2015").fadeTo("slow", 1);
+            }
+        });
+    })
+    .catch((err) => {
+        console.log("Lo sentimos algo falló, intentalo nuevamente", err);
+    });
+
+// .catch((err) => {
+//     console.log("Lo sentimos algo falló", err);
+//     document.getElementById("portfolio").innerHTML +=
+//     '<div class="error text-center fs-1"><p>Ups! Ocurrió un error :-(</p></div>';
+// });
